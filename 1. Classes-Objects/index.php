fae3ddec9c832f9d8b9e3b6702413b28cb4ad2d6
bappi2097 <bappi35-2097@diu.edu.bd>
2020-10-15 13:12:14 +0600

    class object complete

--- 1. Classes-Objects/index.php
+++ 1. Classes-Objects/index.php
@@ -1,5 +1,40 @@
 <?php
 
+class Car
+{
+    public $name;
+    public $color;
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getColor()
+    {
+        return $this->color;
+    }
+    function setColor($color)
+    {
+        $this->color = $color;
+    }
+}
+
+$car1 = new Car();
+$car2 = new Car();
+// var_dump($car1 instanceof Car);
+$car1->setName("volvo");
+$car1->setColor("black");
+$car2->name = "audi";
+$car2->color = "blue";
+
+echo "Car-1: Name: " . $car1->getName() . " Color: " . $car1->getColor() . "</br>";
+echo "Car-2: Name: " . $car2->getName() . " Color: " . $car2->getColor() . "</br>";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
